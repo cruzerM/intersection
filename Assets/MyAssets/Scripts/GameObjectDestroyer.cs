1247159c149ac5e5551cf579d0efafe71c2a9335
cruzerM <mahmoud.amri@outlook.com>
2015-09-18 12:44:08 +0100

    first push

---
+++ Assets/MyAssets/Scripts/GameObjectDestroyer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjectDestroyer : MonoBehaviour {
+
+    void OnTriggerEnter(Collider collider)
+    {
+		Debug.Log ("before Destroy");
+		Destroy(collider.gameObject);
+    }
+}
